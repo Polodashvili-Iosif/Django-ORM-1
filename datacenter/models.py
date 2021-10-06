6e32b3e27d2c44d12d658ad31e12b305bcabb2d3
Killer_Bi_ <iosif.polodashvili@mail.ru>
2021-10-06 20:00:00 +0300

    - Подобрал точные названия и добавил декоратор к статическому методу format_duration

--- datacenter/models.py
+++ datacenter/models.py
@@ -23,26 +23,27 @@ class Visit(models.Model):
     entered_at = models.DateTimeField()
     leaved_at = models.DateTimeField(null=True)
 
-    def get_duration(visit):
-        if visit.leaved_at is None:
-            entered_time = localtime(value=visit.entered_at,
+    def get_duration(self):
+        if self.leaved_at is None:
+            entered_time = localtime(value=self.entered_at,
                                      timezone=pytz.timezone('Europe/Moscow')
                                      ).replace(tzinfo=None)
             leave_time = datetime.datetime.now()
         else:
-            entered_time = visit.entered_at
-            leave_time = visit.leaved_at
+            entered_time = self.entered_at
+            leave_time = self.leaved_at
         seconds_delta = (leave_time - entered_time).total_seconds()
         return seconds_delta
 
+    @staticmethod
     def format_duration(duration):
         seconds = int(duration % 60)
         minutes = int(duration % 3600 // 60)
         hours = int(duration // 3600)
         return f'{hours:02d}:{minutes:02d}:{seconds:02d}'
 
-    def is_long(visit, minutes=60):
-        return visit.get_duration() / 60 > minutes
+    def is_long(self, minutes=60):
+        return self.get_duration() / 60 > minutes
 
     def __str__(self):
         return '{user} entered at {entered} {leaved}'.format(
